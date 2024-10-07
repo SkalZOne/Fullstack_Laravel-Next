f92c80ffc4347f101abd63c714fcc4bffb7d970d
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-07 13:04:19 +0500

    add PostController added show function to PostController; added route to PostController show function

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\FilterRequest;
+use App\Http\Resources\PostsResource;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+
+    public function show() {
+        
+        dd(123);
+    }
+
+}
