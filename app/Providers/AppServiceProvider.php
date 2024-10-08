c52a827484565fc3bfca494cb056ef99c644773e
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-08 13:50:11 +0500

    add onlyOneUrl validator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Validator::extend('onlyOneUrl', function($attribute, $value, $parameters, $validator) {
+            if (!(substr_count($value, "//") >=  2)) {
+                return true;
+            }
+        });
     }
 }
