16916ff33faa697c488e78e95b5b25a108fa4f5c
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-08 12:50:18 +0500

    add user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create();
 
         Post::factory()->count(10)->create();
     }
