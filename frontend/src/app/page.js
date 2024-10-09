169a2decc073b2517b6b829c1118d3d261e33e5e
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-09 14:07:52 +0500

    frontend: add Button component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,10 @@
+import Button from "./components/Button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-xl">Home page</h1>
+      <Button>Login</Button>
     </div>
   );
 }
