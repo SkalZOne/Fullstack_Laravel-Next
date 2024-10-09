3ab9f88f6ef1e3c494b0ca168d88f9f386fa76cd
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-09 09:13:16 +0500

    add tests filterPriceDesc and filterPriceAsc

---
+++ tests/Unit/Services/Post/ServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\Services\Post;
+
+use App\Models\Post;
+use Tests\TestCase;
+
+class ServiceTest extends TestCase
+{
+    private $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->service = new \App\Services\Post\Service;
+    }
+
+    /**
+     * A basic unit test example.
+     */
+    public function test_filterPriceDesc(): void
+    {
+        $createdPosts = Post::factory()->count(2)->create()->toQuery();
+        $result = $this->service->filter(['filter' => 'priceDesc'], $createdPosts)->all();
+
+        $this->assertGreaterThan($result[1]->price, $result[0]->price);
+    }
+
+    public function test_filterPriceAsc(): void
+    {
+        $createdPosts = Post::factory()->count(2)->create()->toQuery();
+        $result = $this->service->filter(['filter' => 'priceAsc'], $createdPosts)->all();
+
+        $this->assertLessThan($result[1]->price, $result[0]->price);
+    }
+}
