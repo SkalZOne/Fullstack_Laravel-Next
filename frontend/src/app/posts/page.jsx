c615422fe16658ee05361bc3c496dfc0c1e0d5be
SkalZ <dima.tysyahnyy@mail.ru>
2024-10-10 19:43:08 +0500

    frontend: add posts search input

--- frontend/src/app/posts/page.jsx
+++ frontend/src/app/posts/page.jsx
@@ -2,24 +2,34 @@
 
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
-import Arrow from "../images/arrow";
+import Arrow from "../images/Arrow";
 import Minus from "../images/Minus";
+import Input from "../components/Input";
 
 const PostAll = () => {
   const [posts, setPosts] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("")
 
   const fetchData = async () => {
-    const response = await fetch(
-      `http://127.0.0.1:8000/getAllPosts/?page=${currentPage}&filter=${filter}`
-    ).then((response) => response.json());
-    setPosts(response);
+    if (search == "") {
+      const response = await fetch(
+        `http://127.0.0.1:8000/getAllPosts/?page=${currentPage}&filter=${filter}`
+      ).then((response) => response.json());
+      setPosts(response);
+    } else {
+      const response = await fetch(
+        `http://127.0.0.1:8000/getSinglePost/?title=${search}`
+      ).then((response) => response.json());
+      setPosts(response);
+    }
   };
 
   useEffect(() => {
     fetchData();
-  }, [currentPage, filter]);
+    console.log(search)
+  }, [currentPage, filter, search]);
 
   const setFilterPrice = () => {
     if (filter !== "priceAsc" && filter !== "priceDesc") setFilter("priceDesc");
@@ -42,15 +52,18 @@ const PostAll = () => {
   };
 
   const getFilterImage = (desc, asc) => {
-    if (filter == desc) return <Arrow className="ml-2"/>
-    if (filter == asc) return <Arrow className="rotate-180 ml-2"/>
-    return <Minus className="ml-2"/>
+    if (filter == desc) return <Arrow className="ml-2"/>;
+    if (filter == asc) return <Arrow className="rotate-180 ml-2"/>;
+    return <Minus className="ml-2" />;
+  };
+
+  const setSearchInput = (event) => {
+    setSearch(event.target.value)
   }
 
   return (
     <div>
       <div className="flex justify-center gap-10 mt-4 mb-10">
-
         <Button onClick={() => setFilter("")}>Clear filters</Button>
 
         <Button onClick={() => setFilterPrice()}>
@@ -62,6 +75,8 @@ const PostAll = () => {
           Sort by date
           {getFilterImage("dateDesc", "dateAsc")}
         </Button>
+
+        <Input placeholder="Search post" onChange={setSearchInput}></Input>
       </div>
       <div className="grid gap-10 grid-cols-5 mx-3">
         {posts &&
@@ -80,34 +95,50 @@ const PostAll = () => {
                 >
                   {primary_photo}
                 </li>
-                <li key={price} className="text-green-700 font-bold">{price}</li>
+                <li key={price} className="text-green-700 font-bold">
+                  {price}
+                </li>
               </ul>
             );
           })}
       </div>
 
-      <div className="flex justify-center items-center gap-14 my-16 mx-24">
-        <Button
-          disabled={posts && currentPage == 1 ? true : false}
-          onClick={() => {
-            setCurrentPage(currentPage - 1);
-          }}
-        >
-          Previous page
-        </Button>
+      {posts?.meta?.links && (
+        <div className="flex justify-center items-center gap-14 my-16 mx-24">
+          <Button
+            disabled={posts && currentPage == 1 ? true : false}
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+            }}
+          >
+            Previous page
+          </Button>
 
-        <div className="flex gap-4">
-          {posts &&
-            posts.meta.links.map(({ active, label }, index) => {
-              if (label == "&laquo; Previous" || label == "Next &raquo;") {
-                return;
-              }
+          <div className="flex gap-4">
+            {posts.meta.links &&
+              posts.meta.links.map(({ active, label }, index) => {
+                if (label == "&laquo; Previous" || label == "Next &raquo;") {
+                  return;
+                }
 
-              if (active == true) {
+                if (active == true) {
+                  return (
+                    <span
+                      key={index}
+                      className="flex justify-center items-center text-white text-center bg-indigo-600 border border-indigo-600 min-w-7 max-h-7 cursor-pointer rounded-full transition-colors hover:bg-white hover:text-black"
+                      onClick={() => {
+                        setCurrentPage(index);
+                      }}
+                    >
+                      {index}
+                    </span>
+                  );
+                }
+
                 return (
                   <span
                     key={index}
-                    className="flex justify-center items-center text-white text-center bg-indigo-600 border border-indigo-600 min-w-7 max-h-7 cursor-pointer rounded-full transition-colors hover:bg-white hover:text-black"
+                    className="flex justify-center items-center text-black text-center bg-white border border-indigo-600 min-w-7 max-h-7 cursor-pointer rounded-full transition-colors hover:bg-indigo-600 hover:text-white"
                     onClick={() => {
                       setCurrentPage(index);
                     }}
@@ -115,33 +146,23 @@ const PostAll = () => {
                     {index}
                   </span>
                 );
-              }
+              })}
+          </div>
 
-              return (
-                <span
-                  key={index}
-                  className="flex justify-center items-center text-black text-center bg-white border border-indigo-600 min-w-7 max-h-7 cursor-pointer rounded-full transition-colors hover:bg-indigo-600 hover:text-white"
-                  onClick={() => {
-                    console.log(currentPage);
-                    setCurrentPage(index);
-                    console.log(currentPage);
-                  }}
-                >
-                  {index}
-                </span>
-              );
-            })}
+          <Button
+            disabled={
+              posts.meta.last_page && currentPage == posts.meta.last_page
+                ? true
+                : false
+            }
+            onClick={() => {
+              setCurrentPage(currentPage + 1);
+            }}
+          >
+            Next page
+          </Button>
         </div>
-
-        <Button
-          disabled={posts && currentPage == posts.meta.last_page ? true : false}
-          onClick={() => {
-            setCurrentPage(currentPage + 1);
-          }}
-        >
-          Next page
-        </Button>
-      </div>
+      )}
     </div>
   );
 };
